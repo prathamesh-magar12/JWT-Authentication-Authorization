932956a22680aa0fcb68fbbd5e03a8ab0614e786
Prathamesh Magar <prathamesh.magar12@gmail.com>
2025-12-26 02:38:18 +0530

    fix: update CORS configuration to allow multiple origins with dynamic handling

--- backend/index.js
+++ backend/index.js
@@ -5,17 +5,38 @@ const cors = require("cors");
 const authRouter = require("./Routes/authRouter");
 const productRouter = require("./Routes/productRouter");
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://jwt-authentication-authorization-one.vercel.app"
+];
+
+// app.use(cors({
+//   origin: allowedOrigins,
+//   credentials: true
+// }));
+
 app.use(cors({
-  origin: "https://jwt-authentication-authorization-one.vercel.app",
-  credentials: true
+  origin: function (origin, callback) {
+    // allow Postman / server-to-server
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
 
 require("dotenv").config();
 require("./Models/db");
 const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
-app.use(cors());
 app.use("/auth", authRouter);
 app.use("/products", productRouter);
 
